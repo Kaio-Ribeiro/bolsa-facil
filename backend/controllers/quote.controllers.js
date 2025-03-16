edf398c9355fc5578de16867c3dee7cefcbabeb5
Kaio Anderson Ribeiro de Oliveira <51684209+Kaio-Ribeiro@users.noreply.github.com>
2025-03-16 13:09:40 +0000

    feat: Adiciona rota de busca de cotação de um ativo financeiro

--- backend/controllers/quote.controllers.js
+++ backend/controllers/quote.controllers.js
@@ -31,7 +31,7 @@ export const listQuotes = async (req, res) => {
       );
     }
 
-    // Converte a resposta par JSON
+    // Converte a resposta para JSON
     const data = await response.json();
 
     // Retorna os dados para o cliente
@@ -44,7 +44,58 @@ export const listQuotes = async (req, res) => {
 
     return res.status(500).json({
       success: false,
-      message: "Erro ao buscar dados da API externa",
+      message: "Erro ao listar ações na API externa",
+      error: error.message,
+    });
+  }
+};
+
+// Função para buscar os dados da cotação de um ativo financeiro
+export const getQuote = async (req, res) => {
+  try {
+    // Obtém o token do arquivo.env
+    const apiToken = process.env.BRAPI_API_TOKEN;
+
+    if (!apiToken) {
+      return res.status(500).json({
+        success: false,
+        error: "Token de API não configurado",
+      });
+    }
+
+    // Obtém a URL da API externa
+    const getQuoteURL = String(process.env.GET_QUOTE_URL) + req.params.tickers;
+
+    // Faz a requisição para a API externa
+    const response = await fetch(getQuoteURL, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    // Verifica se a resposta é bem-sucedida
+    if (!response.ok) {
+      throw new Error(
+        `Erro na API externa: ${response.status} ${response.statusText}`
+      );
+    }
+
+    // Converte a resposta para JSON
+    const data = await response.json();
+
+    // Retorna os dados para o cliente
+    return res.status(200).json({
+      succes: true,
+      data,
+    });
+  } catch (error) {
+    console.error("Erro ao chamar API externa:", error.message);
+
+    res.status(500).json({
+      success: false,
+      message: "Erro ao buscar dados da ação na API externa",
       error: error.message,
     });
   }
--- backend/routes/quote.routes.js
+++ backend/routes/quote.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import { listQuotes } from "../controllers/quote.controllers.js";
+import { listQuotes, getQuote } from "../controllers/quote.controllers.js";
 
 const router = express.Router();
 
 // Rota para listar cotações
-router.get("/quote/list", listQuotes);
+router.get("/quotes/list", listQuotes);
+
+// Rota para buscar cotação de um ativo financeiro
+router.get("/quote/:tickers", getQuote);
 
 export default router;
